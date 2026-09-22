07215844c07cba99d827baa8f35ffc161c36b5f6
Pedro Guimaraes <peuvictor22@gmail.com>
2026-09-22 15:00:48 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/io/github/peuvictor/budgeting/DioBudgetingGeminiApplication.java
@@ -0,0 +1,13 @@
+package io.github.peuvictor.budgeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioBudgetingGeminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioBudgetingGeminiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/peuvictor/budgeting/DioBudgetingGeminiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.peuvictor.budgeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioBudgetingGeminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
