55800efbd8d3e6919e735977ce11e29ab10b19d1
Pedro Guimaraes <peuvictor22@gmail.com>
2026-09-23 10:01:47 -0300

    feat: add ChatClient REST endpoint

--- src/main/java/io/github/peuvictor/budgeting/DioBudgetingGeminiApplication.java
+++ src/main/java/io/github/peuvictor/budgeting/DioBudgetingGeminiApplication.java
@@ -1,13 +1,19 @@
 package io.github.peuvictor.budgeting;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DioBudgetingGeminiApplication {
 
+	@Bean
+	ChatClient chatClient(ChatClient.Builder builder) {
+		return builder.build();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DioBudgetingGeminiApplication.class, args);
 	}
-
 }
